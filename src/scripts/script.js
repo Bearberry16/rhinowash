cb062071191b94dcdfd25ade5a95f2737b78185a
Katarina Ljungdahl <katarina@folkungagatan54.se>
2017-06-12 09:23:26 +0200

    Added new file for animation functionality

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,7 @@
 // Only imported for demo purposes
 import { createNode, pad } from './functions';
 import scroll from './labb';
+import animation from './animation';
 
 scroll(); // Initializes all functionality in labb.js.
+animation(); // Initializes Vivus functionality.
