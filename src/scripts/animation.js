ba526906b5f26c88d3c57acf40e9bacf3a145b5c
Katarina Ljungdahl <katarina@folkungagatan54.se>
2017-06-12 10:07:02 +0200

    Added Vivus animation library, only placeholder svg

--- src/scripts/animation.js
+++ src/scripts/animation.js
@@ -1,3 +1,96 @@
 export default function animation() {
-  console.log('hello!');
+  /*----------------------------------------------------------------------------
+    ANIMATION 1 (ANIMATES WHEN IN VIEWPORT)
+  ----------------------------------------------------------------------------*/
+  function piggy() {
+    new Vivus('piggy-bank', {
+      duration: 150,
+      animTimingFunction: Vivus.EASEOUT,
+      type: 'oneByOne'
+    });
+  }
+  piggy();
+
+  /*----------------------------------------------------------------------------
+    ANIMATION 2
+  ----------------------------------------------------------------------------*/
+  function piggy2() {
+    new Vivus('piggy-bank-2', {
+      duration: 150,
+      animTimingFunction: Vivus.EASEOUT,
+      type: 'oneByOne'
+    });
+  }
+
+  piggy2();
+
+  /*----------------------------------------------------------------------------
+    ANIMATION 3
+  ----------------------------------------------------------------------------*/
+  function piggy3() {
+    new Vivus('piggy-bank-3', {
+      duration: 150,
+      animTimingFunction: Vivus.EASEOUT,
+      type: 'oneByOne'
+    });
+  }
+  piggy3();
+
+  /*----------------------------------------------------------------------------
+    ANIMATION 4
+  ----------------------------------------------------------------------------*/
+  function piggy4() {
+    new Vivus('piggy-bank-4', {
+      duration: 150,
+      animTimingFunction: Vivus.EASEOUT,
+      type: 'oneByOne'
+    });
+  }
+  piggy4();
+
+  /*----------------------------------------------------------------------------
+    ANIMATION 5
+  ----------------------------------------------------------------------------*/
+  function piggy5() {
+    new Vivus('piggy-bank-5', {
+      duration: 150,
+      animTimingFunction: Vivus.EASEOUT,
+      type: 'oneByOne'
+    });
+  }
+  piggy5();
+  /*----------------------------------------------------------------------------
+    ANIMATION 6
+  ----------------------------------------------------------------------------*/
+  function piggy6() {
+    new Vivus('piggy-bank-6', {
+      duration: 150,
+      animTimingFunction: Vivus.EASEOUT,
+      type: 'oneByOne'
+    });
+  }
+  piggy6();
+
+  /*----------------------------------------------------------------------------
+    ANIMATION 7
+  ----------------------------------------------------------------------------*/
+  function piggy7() {
+    new Vivus('piggy-bank-7', {
+      duration: 150,
+      animTimingFunction: Vivus.EASEOUT,
+      type: 'oneByOne'
+    });
+  }
+  piggy7();
+  /*----------------------------------------------------------------------------
+    ANIMATION 8
+  ----------------------------------------------------------------------------*/
+  function piggy8() {
+    new Vivus('piggy-bank-8', {
+      duration: 150,
+      animTimingFunction: Vivus.EASEOUT,
+      type: 'oneByOne'
+    });
+  }
+  piggy8();
 }
