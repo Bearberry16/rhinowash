f527e792e5ab00561622c447ec8172be08cc08df
Katarina Ljungdahl <katarina@folkungagatan54.se>
2017-06-13 12:53:40 +0200

    Fixed layout on smaller screens, removed overall scroll since it is not working properly

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,7 +1,5 @@
 import animation from './animation';
-import overallScroll from './overall-scroll';
 import scroll from './labb';
 
 animation(); // Initializes Vivus functionality.
 scroll(); // Initializes all functionality in labb.js.
-overallScroll(); // Initializes the overall scroll functionality.
